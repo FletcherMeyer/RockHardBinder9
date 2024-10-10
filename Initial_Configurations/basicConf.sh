3354e0dcb7d8acc4943725065391f64e323f5f83
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2024-10-09 21:20:14 -0500

    Create basicConf.sh

---
+++ Initial_Configurations/basicConf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set Default to CLI
+# TODO: Prompt the engineer if they wish to do this.
+# systemctl set-default multi-user.target
+
+# Update and upgrade.
+# TODO: Include necessary packages the engineer may use for an inject or security configuration.
+# BIND9 is not included. It is in a separate script.
+
+# debootstrap is an important package we will be using to 'jail' our BIND9 service.
+# If there does seem to be a vulnurability through BIND9, this serves to mitigate any damage done to exclusively the service.
+apt install debootstrap
+
+apt-get update
+apt-get upgrade
