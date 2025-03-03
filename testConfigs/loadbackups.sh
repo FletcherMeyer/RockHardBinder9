9f76fc4b12f4e1d2e97b57367eb4c23a523bfa6b
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-03-02 20:35:23 -0600

    Update loadbackups.sh

--- testConfigs/loadbackups.sh
+++ testConfigs/loadbackups.sh
@@ -19,7 +19,7 @@ fi
 if [ -d "$backup_dir" ]; then
     echo -e "[${GREEN}OKAY${RESET}] Backup found..."
 else
-    echo -e "[${RED}FAILURE${RESET}] Backup directory 'backup_dir' not in file system... Unable to fetch files."
+    echo -e "[${RED}FAILURE${RESET}] Backup directory '$backup_dir' not in file system... Unable to fetch files."
     exit 1
 fi
 
@@ -34,7 +34,13 @@ while IFS= read -r file; do
     # Check if the file exists before copying
     if [ -f "$backup_dir/$file" ] || [ -d "$backup_dir/$file" ]; then
         cp -r "$backup_dir/$file" "$file"
-        echo -e "[${GREEN}OKAY${RESET}] Backup restored for: $file"
+
+        if [ $? -ne 0 ]; then
+            echo -e "[${RED}FAILURE${RESET}] Unable to copy: $backup_dir/$file"
+        else
+            echo -e "[${GREEN}OKAY${RESET}] Backup restored for: $backup_dir/$file"
+        fi
+
     else
         echo -e "[${RED}FAILURE${RESET}] File or Directory not found: $file"
     fi
