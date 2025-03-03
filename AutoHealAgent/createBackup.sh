4eee33ead2986793d41f3372025120946358bdc2
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-03-02 20:40:50 -0600

    Create createBackup.sh

---
+++ AutoHealAgent/createBackup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+if [[ "$EUID" -ne 0 ]]
+then
+    printf 'Must be run as root, exiting!\n'
+    exit 1
+fi
+
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
+echo "Please enter the location of the backup directory [q to exit] : "
+read backup_dir
+if [ "$backup_dir" == "q" ]; then
+    echo -e "[${GREEN}OKAY${RESET}] Exiting"
+    exit 0
+fi
+if [ -d "$backup_dir" ]; then
+    echo -e "[${GREEN}OKAY${RESET}] Backup found..."
+else
+    echo -e "[${YELLOW}WARNING${RESET}] Backup directory 'backup_dir' not in file system. Creating..."
+    mkdir -p "$backup_dir"
+fi
+
+# Define the file that contains the list of files to back up
+echo "Please enter the location of the file list [q to exit] : "
+read file_list
+if [ "$file_list" == "q" ]; then
+    echo -e "[${GREEN}OKAY${RESET}] Exiting"
+    exit 0
+fi
+if [ -f "$file_list" ]; then
+    echo -e "[${GREEN}OKAY${RESET}] File list found..."
+else
+    echo -e "[${RED}FAILURE${RESET}] File list not found. Exiting."
+    exit 0
+fi
+
+# Create the backup directory if it does not exist
+mkdir -p "$backup_dir"
+
+# Read the file list line by line
+while IFS= read -r file; do
+    # Check if the file exists before copying
+    if [ -f "$file" ] || [ -d "$file" ]; then
+        file_dir="$(dirname $file)"
+        mkdir -p "$backup_dir$file_dir"
+        cp -r "$file" "$backup_dir$file"
+        if [ $? -ne 0 ]; then
+            echo -e "[${RED}FAILURE${RESET}] Unable to backup: $file"
+        else
+            echo -e "[${GREEN}OKAY${RESET}] Backup made for: $file"
+        fi
+
+    else
+        echo -e "[${RED}FAILURE${RESET}] File or Directory not found: $file"
+    fi
+done < "$file_list"
+
+echo -e "[${GREEN}OKAY${RESET}] Backup completed."
