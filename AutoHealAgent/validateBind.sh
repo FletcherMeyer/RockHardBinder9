07a8429c3700918a8e79cb1fa38646f7839750e1
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-03-02 20:56:34 -0600

    Update validateBind.sh

--- AutoHealAgent/validateBind.sh
+++ AutoHealAgent/validateBind.sh
@@ -11,7 +11,7 @@ RED="\e[31m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-file_name=${1:/records.txt}
+file_name=${1:-"./records.txt"}
 if [ -f "$file_name" ]; then
     echo -e "[${GREEN}OKAY${RESET}] Records file found..."
 else
@@ -19,8 +19,7 @@ else
     # If it is a '2', we know it is something pertaining to the Auto Healing Agent that is broken.
     exit 2
 fi
-
-source file_name
+source "$file_name"
 
 echo -e "Checking domain resolving..."
 
@@ -31,7 +30,6 @@ do
 	if [ "${RESOLVE_ARR[i]}" == "${IPADDR}" ]; then
     echo -e "[${GREEN}OKAY${RESET}] ${DOMAIN_ARR[i]} resolved!"
     # It will check for a '0' to determine success.
-		exit 0;
 	else
     echo -e "[${GREEN}RED${RESET}] ${DOMAIN_ARR[i]} did not resolve..."
     # It will check for a '0' to determine success.
@@ -39,3 +37,5 @@ do
 	fi
 	echo "	${IPADDR}"
 done
+
+exit 0;
