07ab2c6e25b59ac957b002f28be09b013d290564
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-03-06 21:56:56 -0600

    Update loadBackups.sh

--- AutoHealAgent/loadBackups.sh
+++ AutoHealAgent/loadBackups.sh
@@ -10,31 +10,39 @@ RED="\e[31m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-echo "Please enter the location of the backup directory [q to exit] : "
-read backup_dir
-if [ "$backup_dir" == "q" ]; then
-    echo -e "[${GREEN}OKAY${RESET}] Exiting"
-    exit 0
-fi
-if [ -d "$backup_dir" ]; then
-    echo -e "[${GREEN}OKAY${RESET}] Backup found..."
+if [ -z "$1" ]; then
+    echo "Please enter the location of the backup directory [q to exit] : "
+    read backup_dir
+    if [ "$backup_dir" == "q" ]; then
+        echo -e "[${GREEN}OKAY${RESET}] Exiting"
+        exit 1
+    fi
+    if [ -d "$backup_dir" ]; then
+        echo -e "[${GREEN}OKAY${RESET}] Backup found..."
+    else
+        echo -e "[${YELLOW}WARNING${RESET}] Backup directory '$backup_dir' not in file system... Making directory.."
+        exit 1
+    fi
 else
-    echo -e "[${RED}FAILURE${RESET}] Backup directory '$backup_dir' not in file system... Unable to fetch files."
-    exit 1
+    backup_dir="$1"
 fi
 
 # Define the file that contains the list of files to back up
-echo "Please enter the location of the file list [q to exit] : "
-read file_list
-if [ "$file_list" == "q" ]; then
-    echo -e "[${GREEN}OKAY${RESET}] Exiting"
-    exit 0
-fi
-if [ -f "$file_list" ]; then
-    echo -e "[${GREEN}OKAY${RESET}] File list found..."
+if [ -z "$2" ]; then
+    echo "Please enter the location of the file list [q to exit] : "
+    read file_list
+    if [ "$file_list" == "q" ]; then
+        echo -e "[${GREEN}OKAY${RESET}] Exiting"
+        exit 1
+    fi
+    if [ -f "$file_list" ]; then
+        echo -e "[${GREEN}OKAY${RESET}] File list found..."
+    else
+        echo -e "[${RED}FAILURE${RESET}] File list not found. Exiting."
+        exit 1
+    fi
 else
-    echo -e "[${RED}FAILURE${RESET}] File list not found. Exiting."
-    exit 1
+    file_list="$2"
 fi
 
 # Create the backup directory if it does not exist
@@ -44,12 +52,12 @@ mkdir -p "$backup_dir"
 while IFS= read -r file; do
     # Check if the file exists before copying
     if [ -f "$backup_dir$file" ] || [ -d "$backup_dir$file" ]; then
-        cp -r "$backup_dir$file" "$file"
+        cp -r "$file" "$backup_dir$file"
 
         if [ $? -ne 0 ]; then
             echo -e "[${RED}FAILURE${RESET}] Unable to copy: $backup_dir$file"
         else
-            echo -e "[${GREEN}OKAY${RESET}] Backup restored for: $backup_dir$file"
+            echo -e "[${GREEN}OKAY${RESET}] Backup made for: $backup_dir$file"
         fi
 
     else
@@ -57,4 +65,5 @@ while IFS= read -r file; do
     fi
 done < "$file_list"
 
-echo -e "[${GREEN}OKAY${RESET}] Backups restored."
+echo -e "[${GREEN}OKAY${RESET}] Backups made."
+exit 0
