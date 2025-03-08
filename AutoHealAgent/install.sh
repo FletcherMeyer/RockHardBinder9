9b7b71c047190182783b077f87d1a09d35801e2a
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2025-03-08 10:31:31 -0600

    Update install.sh

--- AutoHealAgent/install.sh
+++ AutoHealAgent/install.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash
 
-touch /usr/sbin/backup
-chmod +x /usr/sbin/backup
+touch /usr/sbin/load
+chmod +x /usr/sbin/load
 
-cat > /usr/sbin/backup << 'EOF'
+cat > /usr/sbin/load << 'EOF'
 #!/bin/bash
 
 if [[ "$EUID" -ne 0 ]]
@@ -17,25 +18,25 @@ RED="\e[31m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "Please enter the location of the backup directory [q to exit] : "
     read backup_dir
     if [ "$backup_dir" == "q" ]; then
         echo -e "[${GREEN}OKAY${RESET}] Exiting"
-        exit 1
+        exit 0
     fi
     if [ -d "$backup_dir" ]; then
         echo -e "[${GREEN}OKAY${RESET}] Backup found..."
     else
-        echo -e "[${YELLOW}WARNING${RESET}] Backup directory '$backup_dir' not in file system. Creating..."
-        mkdir -p "$backup_dir"
+        echo -e "[${RED}WARNING${RESET}] Backup directory '$backup_dir' not in file system... Exiting."
+        exit 1
     fi
 else
     backup_dir="$1"
 fi
 
 # Define the file that contains the list of files to back up
-if [ -z $2 ]; then
+if [ -z "$2" ]; then
     echo "Please enter the location of the file list [q to exit] : "
     read file_list
     if [ "$file_list" == "q" ]; then
@@ -45,31 +46,31 @@ if [ -z $2 ]; then
     if [ -f "$file_list" ]; then
         echo -e "[${GREEN}OKAY${RESET}] File list found..."
     else
-        echo -e "[${RED}FAILURE${RESET}] File list not found. Exiting with 2."
-        exit 2
+        echo -e "[${RED}FAILURE${RESET}] File list not found. Exiting."
+        exit 1
     fi
 else
     file_list="$2"
 fi
 
+# Create the backup directory if it does not exist
+mkdir -p "$backup_dir"
+
 # Read the file list line by line
 while IFS= read -r file; do
     # Check if the file exists before copying
-    if [ -f "$file" ] || [ -d "$file" ]; then
-        file_dir="$(dirname $file)"
-        mkdir -p "$backup_dir$file_dir"
-        cp -r "$file" "$backup_dir$file"
+    if [ -f "$backup_dir$file" ] || [ -d "$backup_dir$file" ]; then
+        cp -r "$backup_dir$file" "$file"
         if [ $? -ne 0 ]; then
-            echo -e "[${RED}FAILURE${RESET}] Unable to backup: $file"
+            echo -e "[${RED}FAILURE${RESET}] Unable to copy: $backup_dir$file"
         else
-            echo -e "[${GREEN}OKAY${RESET}] Backup made for: $file"
-        fi
-
+            echo -e "[${GREEN}OKAY${RESET}] Backup loaded for: $backup_dir$file"
+        fis
     else
         echo -e "[${RED}FAILURE${RESET}] File or Directory not found: $file"
     fi
 done < "$file_list"
 
-echo -e "[${GREEN}OKAY${RESET}] Backup completed."
+echo -e "[${GREEN}OKAY${RESET}] Backups loaded."
 exit 0
 EOF
